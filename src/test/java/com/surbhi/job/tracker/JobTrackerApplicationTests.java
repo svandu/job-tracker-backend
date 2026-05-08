b1a2012c109d5d8eaccabdc94a9cfca92b166cda
svandu <svandu08@gmail.com>
2026-05-08 18:14:05 +0530

    Initial Spring Boot project setup

---
+++ src/test/java/com/surbhi/job/tracker/JobTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.surbhi.job.tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
